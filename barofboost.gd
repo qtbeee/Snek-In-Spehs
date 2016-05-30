da3d0bfdae057fae1628b740ae888e99ab43ab35
qtbeee <kbrambil@ucsc.edu>
2016-05-29 20:11:49 -0700

    hud now moves with screen resizing

--- barofboost.gd
+++ barofboost.gd
@@ -1,16 +1,19 @@
 
 extends Panel
 
-onready var snek = get_parent().get_parent().get_parent().get_node("Snek")
+onready var snek = get_tree().get_root().get_child(0).get_node("Snek")
+onready var control = get_parent()
 var width = 200
 
 const SCALE = 2
-const height = 42
+const HEIGHT = 42
 
 func _ready():
 	set_fixed_process(true)
 
 func _fixed_process(delta):
+	var height = get_viewport().get_rect().size.height - HEIGHT
+	get_parent().set_pos(Vector2(control.get_pos().x, height))
 	width = snek.get("b") * SCALE
 	set_size(Vector2(width, height))
 
--- level.gd
+++ level.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 #var camera
 onready var snake = get_node("Snek")
-onready var deathmsg = get_node("HUD").get_node("DeathMessage")
+onready var deathmsg = get_node("HUD/Control/DeathMessage")
 
 func _on_Snek_death():
 	deathmsg.set_text("YOU DIED")
