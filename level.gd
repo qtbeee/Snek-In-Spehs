5cd0bfa990bfc268f7155a3ed737ef5d68882fb2
qtbeee <kbrambil@ucsc.edu>
2016-06-02 18:56:46 -0700

    improvements ahoy
    font-ified labels
    added tutorial stuff to first room
    can now restart game on death or win, and also quit

--- level.gd
+++ level.gd
@@ -7,8 +7,17 @@ onready var winstate = get_node("WinningStuff")
 
 func _on_Snek_death():
 	deathmsg.set_text("YOU DIED")
+	get_node("HUD/Quit").show()
+	get_node("HUD/Restart").show()
 
 func _on_Snek_win():
-	deathmsg.set_text("YOU WIN!")
+	deathmsg.set_text("!")
 	winstate.get_node("Particles2D").set_emitting(true)
-	
+	get_node("HUD/Quit").show()
+	get_node("HUD/Restart").show()
+
+func _on_Restart_pressed():
+	get_tree().reload_current_scene()
+
+func _on_Quit_pressed():
+	get_tree().quit()
